3e99821e6447f70b8de87f0d14ed3198bb5bd1ae
Mar Bert John Mataverde <marbertmataverde.dev@gmail.com>
2022-12-26 15:32:42 +0800

    connect firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wiser/firebase_options.dart';
 import 'package:wiser/wiser.dart';
 
-void main() {
-  runApp(const ProviderScope(child: WiserApp()));
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const ProviderScope(
+      child: WiserApp(),
+    ),
+  );
 }
