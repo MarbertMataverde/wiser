1ba48bb93e24b901b49add37f0f3f655d34e2bbe
Mar Bert John Mataverde <marbertmataverde.dev@gmail.com>
2022-12-26 15:32:19 +0800

    fix login validator typo
    fix typo in passwordValidator if the field is empty return "Please enter an email" to "Please enter an password"

--- lib/features/authentication/login/validator/validator.dart
+++ lib/features/authentication/login/validator/validator.dart
@@ -2,7 +2,7 @@ import 'package:wiser/core/constant.dart';
 
 String? passwordValidator(value) {
   if (value!.isEmpty) {
-    return 'Please enter an email';
+    return 'Please enter an password';
   } else if (value.length < 8) {
     return 'Password must be at least 8 characters';
   } else if (!value.contains(RegExp(r'[A-Z]'))) {
