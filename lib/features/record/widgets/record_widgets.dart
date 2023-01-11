d09346b2c61fa4974d195beca41acb43be1572e8
Mar Bert John Mataverde <marbertmataverde.dev@gmail.com>
2023-01-11 23:47:35 +0800

    new record select account functionality

--- lib/features/record/widgets/record_widgets.dart
+++ lib/features/record/widgets/record_widgets.dart
@@ -3,3 +3,4 @@ export 'record_category_card_widget.dart';
 export 'record_selected_category_widget.dart';
 export 'record_sub_category_card_widget.dart';
 export 'record_transaction_type_bar_widget.dart';
+export 'record_show_select_account_dialog_widget.dart';
