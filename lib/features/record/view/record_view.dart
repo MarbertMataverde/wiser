ac2d8b0f339148d28fe54573397cbd043a7be2f0
Mar Bert John Mataverde <marbertmataverde.dev@gmail.com>
2023-01-12 18:39:34 +0800

    update transaction page to record page

--- lib/features/record/view/record_view.dart
+++ lib/features/record/view/record_view.dart
@@ -7,7 +7,7 @@ class TransactionsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('T R A N S A C T I O N S'),
+        child: Text('R E C O R D S'),
       ),
     );
   }
